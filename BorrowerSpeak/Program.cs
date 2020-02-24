cff2b668ae2dd90ddc0ee2c1e4969b770e695cac
Сергей Гупалов <asterak@gmail.com>
2020-02-24 09:45:55 +0300

    Add project files.

---
+++ BorrowerSpeak/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BorrowerSpeak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Borrow("WhAt! FiCK! DaMn CAke?")); // whatfickdamncake
+            Console.WriteLine(Borrow("THE big PeOpLE Here!!")); // thebigpeoplehere
+            Console.WriteLine(Borrow("i AM a TINY BoY!!"));  // iamatinyboy
+
+        }
+
+        public static string Borrow(string s) => Regex.Replace(s.ToLower(), @"[^\w]", "");
+       
+    }
+}
